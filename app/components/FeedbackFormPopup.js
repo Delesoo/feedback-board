a72481c7e254fd7f0a44ab75b8363d55c0eedab2
Delesoo <strongtoka@gmail.com>
2024-01-23 14:46:18 +0400

    upload animation

--- app/components/FeedbackFormPopup.js
+++ app/components/FeedbackFormPopup.js
@@ -4,11 +4,13 @@ import Popup from "./Popup";
 import axios from "axios";
 import Paperclip from "./icons/Paperclip";
 import Trash from "./icons/Trash";
+import {MoonLoader} from 'react-spinners';
 
 export default function FeedbackFormPopup({setShow}) {
   const [title,setTitle] = useState('');
   const [description,setDescription] = useState('');
   const [uploads, setUploads] = useState([]);
+  const [isUploading, setIsUploading] = useState(false);
   function handleCreatePostButtonClick(ev) {
     ev.preventDefault();
     axios.post('/api/feedback', {title, description})
@@ -18,6 +20,7 @@ export default function FeedbackFormPopup({setShow}) {
   }
   async function handleAttachFilesInputChange(ev) {
     const files = [...ev.target.files];
+    setIsUploading(true);
     const data = new FormData();
     for (const file of files) {
       data.append('file', file);
@@ -26,6 +29,7 @@ export default function FeedbackFormPopup({setShow}) {
     setUploads((existingUpload) => {
       return [...existingUpload, ...res.data]
     });
+    setIsUploading(false);
   }
   function handleRemoveFileButtonClick(ev, link) {
     ev.preventDefault();
@@ -74,8 +78,11 @@ export default function FeedbackFormPopup({setShow}) {
               )}
 
               <div className="flex gap-2 mt-2 justify-end">
-                <label className="py-2 px-4 text-gray-600 cursor-pointer">
-                  <span>Attach files</span>
+                <label className={"flex gap-2 py-2 px-4 text-gray-600 cursor-pointer"}>
+                  {isUploading && (
+                    <MoonLoader size={18} />
+                  )}
+                  <span className={(isUploading ? 'text-gray-300' : '')}>{isUploading ? 'Uploading...' : 'Attach files'}</span>
                   <input multiple onChange={handleAttachFilesInputChange} type="file" className="hidden"></input>
                 </label>
                 <Button primary onClick={handleCreatePostButtonClick}>Create a post</Button>

1d81fcf1da0f548db0b13a0b0ee7a7b87390c63a
Delesoo <strongtoka@gmail.com>
2024-02-12 14:36:18 +0400

    minor changes

--- app/api/feedback/route.js
+++ app/api/feedback/route.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 import {Feedback} from "@/app/models/Feedback"
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST(request) {
    const jsonBody = await request.json();
    const {title, description, uploads} = jsonBody;
    const mongoUrl = process.env.MONGO_URL;
    mongoose.connect(mongoUrl);
-   await Feedback.create({title, description, uploads});
+   const session = await getServerSession(authOptions);
+   const userEmail = session.user.email;
+   await Feedback.create({title, description, uploads, userEmail});
    return Response.json(jsonBody);
 }
 
--- app/models/Feedback.js
+++ app/models/Feedback.js
@@ -4,6 +4,7 @@ const feedbackSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String},
     uploads: {type: [String]},
+    userEmail: {type: String, required: true},
 }, {timestamps: true});
 
 export const Feedback = models?.Feedback || model('Feedback', feedbackSchema);
